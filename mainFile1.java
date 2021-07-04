e4778521c7f93ad6b9ae4ba1af3194f3c67d2cf6
Abul Fazal Saif <saif.g12@gmail.com>
2021-07-04 10:18:20 +0530

    added few files and written code for userInput in mainFile2.java

---
+++ mainFile1.java
@@ -0,0 +1,5 @@
+class mainFile1{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ mainFile2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class mainFile2{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            
+        }
+
+        sc.close();
+    }
+}
